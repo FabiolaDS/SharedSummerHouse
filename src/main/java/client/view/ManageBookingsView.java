d4550722dac9fb55c1ce762b24d82e0b2387738f
Fabiola <285055@via.dk>
2021-05-04 17:07:26 +0200

    New project set up (initial commit)
    Created fxml's and view classes for manage bookings and review summerhouse

---
+++ src/main/java/client/view/ManageBookingsView.java
@@ -0,0 +1,29 @@
+package client.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class ManageBookingsView {
+
+    @FXML
+    private ListView<?> pastBookings;
+
+    @FXML
+    private ListView<?> currentBookings;
+
+    @FXML
+    void cancelBooking(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reviewBooking(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewBooking(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/client/view/ReviewSummerhouseView.java
@@ -0,0 +1,31 @@
+package client.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+
+public class ReviewSummerhouseView {
+
+    @FXML
+    private Slider rating;
+
+    @FXML
+    private TextArea comment;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSubmit(ActionEvent event) {
+
+    }
+}
+
